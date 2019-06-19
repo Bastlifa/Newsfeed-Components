352c23887ba1827dde8cfacebb70bd8b90d18bd5
Eli Sacks <bastlifa@gmail.com>
2019-06-19 12:26:35 -0400

    added 'read' button to article to remove it (Strch 2)

--- Article/Article.js
+++ Article/Article.js
@@ -7,15 +7,24 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", this.expandArticle.bind(this));
-  }
 
-  expandArticle() {
+    this.readButton = domElement.querySelector(".readButton");
+    this.readButton.textContent = "read";
+    this.readButton.addEventListener("click", this.removeArticle.bind(this));
+  }
+  expandArticle() 
+  {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
-    this.expandButton.textContent === "expand" ? this.expandButton.textContent = "collapse" : this.expandButton.textContent = "expand";
+    this.readButton.classList.toggle("readBtnHide")
+    this.expandButton.textContent === "Click to Expand" ? this.expandButton.textContent = "Click to Collapse" : this.expandButton.textContent = "Click to Expand";
+  }
+  removeArticle()
+  {
+    this.domElement.style.display = "none";
   }
 }
 
