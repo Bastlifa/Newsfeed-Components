0266c1ad8c2eb625fd4722be6ce2a741ec570310
Eli Sacks <bastlifa@gmail.com>
2019-06-19 13:47:31 -0400

    did all the menu stuff, in ugly way

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,43 @@
+// There's got to be cleaner logic...
+
+const menuOpen = () =>
+{
+  if(Array.from(menu.classList).includes("menu--open"))
+  {
+    TweenMax.to(".menu--open", 1, 
+    {
+      x: 350,
+      ease: Power3.easeOut
+    })
+  }
+}
+
+const menuClose = () =>
+{
+  if(Array.from(menu.classList).includes("menu--open"))
+  {
+    TweenMax.to(".menu", 1, 
+    {
+      x: 0,
+      ease: Power2.easeOut
+    })
+    menu.classList.toggle("menu--open");
+  }
+}
+
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle("menu--open");
+  console.log(menu.classList)
+  if(!Array.from(menu.classList).includes("menu--open"))
+  {
+    menu.classList.toggle("menu--open");
+    menuOpen()
+  }
+  else
+  {
+    menuClose();
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -9,10 +45,33 @@ const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", function() {toggleMenu(); 
-  TweenMax.from(".menu--open", 1, 
+menuButton.addEventListener("click", function() {toggleMenu();});
+
+document.addEventListener("click", function()
+{
+  if (!Array.from(event.target.classList).includes("menu") && !Array.from(event.target.classList).includes("menu-button"))
   {
-    x: -350,
-    ease: Power3.easeOut
-  })
-});
+    menuOpen();
+    menuClose();
+  }
+})
+
+
+//if (!Array.from(menu.classList).includes("menu--open"))
+// {
+//   menu.classList.toggle("menu--open");
+//   TweenMax.from(".menu--open", 1, 
+//   {
+//     x: -350,
+//     ease: Power3.easeOut
+//   })
+// }
+// else
+// {
+//   TweenMax.to($(".menu"), 1, 
+//   {
+//     x: -350,
+//     ease: Power3.easeOut
+//   })
+//   menu.classList.toggle("menu--open");
+// }
